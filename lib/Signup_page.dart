9ae7d11b96f0f00e7c344f5a75a0fd83ccec3024
sarasameh20 <107917078+sarasameh20@users.noreply.github.com>
2025-10-15 21:06:51 +0300

    Update Signup_page.dart

--- lib/Signup_page.dart
+++ lib/Signup_page.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SignUpScreen(),
-    );
-  }
-}
-
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
